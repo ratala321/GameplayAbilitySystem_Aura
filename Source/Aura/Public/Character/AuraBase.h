05875326f2387dfa354cc90abe29491b2014f1d5
Raphaël Talaïa <raphael.talaia@protonmail.com>
2025-04-25 12:00:10 -0400

    6 - Add base aura and enemy character

---
+++ Source/Aura/Public/Character/AuraBase.h
@@ -0,0 +1,17 @@
+
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Character/AuraCharacterBase.h"
+#include "AuraBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API AAuraBase : public AAuraCharacterBase
+{
+	GENERATED_BODY()
+	
+};
