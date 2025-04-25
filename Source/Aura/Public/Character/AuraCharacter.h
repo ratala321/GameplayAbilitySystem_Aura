7ee01fcc3c5f2c41018677ce4007c06bb5153ec1
Raphaël Talaïa <raphael.talaia@protonmail.com>
2025-04-25 12:23:01 -0400

    7 - Add aura and goblin spear blueprint

--- Source/Aura/Public/Character/AuraCharacter.h
+++ Source/Aura/Public/Character/AuraCharacter.h
@@ -4,13 +4,13 @@
 
 #include "CoreMinimal.h"
 #include "Character/AuraCharacterBase.h"
-#include "AuraBase.generated.h"
+#include "AuraCharacter.generated.h"
 
 /**
  * 
  */
 UCLASS()
-class AURA_API AAuraBase : public AAuraCharacterBase
+class AURA_API AAuraCharacter : public AAuraCharacterBase
 {
 	GENERATED_BODY()
 	
