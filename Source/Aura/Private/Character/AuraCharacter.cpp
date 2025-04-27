cef46541fc03c5fa9739cff8ec8f3a9c5b775136
Raphaël Talaïa <raphael.talaia@protonmail.com>
2025-04-27 01:17:27 -0400

    12 - Add gamemode and adjust character animations

--- Source/Aura/Private/Character/AuraCharacter.cpp
+++ Source/Aura/Private/Character/AuraCharacter.cpp
@@ -2,4 +2,17 @@
 
 
 #include "Character/AuraCharacter.h"
+#include "GameFramework/CharacterMovementComponent.h"
 
+AAuraCharacter::AAuraCharacter()
+{
+	UCharacterMovementComponent* AuraCharacterMovement = GetCharacterMovement();
+	AuraCharacterMovement->bOrientRotationToMovement = true;
+	AuraCharacterMovement->RotationRate = FRotator(0.f, 400.f, 0.f);
+	AuraCharacterMovement->bConstrainToPlane = true;
+	AuraCharacterMovement->bSnapToPlaneAtStart = true;
+
+	bUseControllerRotationPitch = false;
+	bUseControllerRotationYaw = false;
+	bUseControllerRotationRoll = false;
+}
