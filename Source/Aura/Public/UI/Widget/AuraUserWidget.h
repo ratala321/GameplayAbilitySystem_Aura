bb2e92d2fbd428d0e3df2c9122012aa69d1442f0
Raphaël Talaïa <raphael.talaia@protonmail.com>
2025-05-01 11:41:35 -0400

    Add aura user widget and widget controller

---
+++ Source/Aura/Public/UI/Widget/AuraUserWidget.h
@@ -0,0 +1,28 @@
+
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "AuraUserWidget.generated.h"
+
+class UAuraWidgetController;
+/**
+ * 
+ */
+UCLASS()
+class AURA_API UAuraUserWidget : public UUserWidget
+{
+	GENERATED_BODY()
+
+public:
+	UFUNCTION(BlueprintCallable)
+	void SetWidgetController(UObject* InWidgetController);
+	
+	UPROPERTY(BlueprintReadOnly)
+	TObjectPtr<UObject> WidgetController;
+	
+protected:
+	UFUNCTION(BlueprintImplementableEvent)
+	void WidgetControllerSet();
+};
